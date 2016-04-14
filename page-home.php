a68b3f6b5d0b3bbcebf121bd1d1afba9275c6c21
Craig Fowler <craig@wearegiants.com>
2016-04-14 12:20:52 -0500

    added map

--- page-home.php
+++ page-home.php
@@ -40,21 +40,26 @@ if( $seoTitle == 'location' ):
 ?>
 
 <div id="<?php echo $seoTitle; ?>" class="home__hero hero  bg--white relative">
-	<div class="centered">
+	<div class="centered centered__bottomer">
 		<div class="fs-row">
-			<div class="fs-cell fs-all-full color--black">
-				<div class="home__hero-copy">
-					<?php the_field('intro_copy'); ?>
-				</div>
+			<div class="fs-cell fs-all-full color--white">
+				<h2 class="home__title title title--xl color--white"><a href="<?php the_permalink(); ?>" class="bg--black"><?php the_title(); ?></a></h2>
 			</div>
 		</div>
 	</div>
 	<div id="gmap" class="covered"></div>
 </div>
+<div class="home__hero hero--padded bg--darkGray">
+	<div class="fs-row">
+		<div class="home__hero-copy fs-cell fs-lg-6 fs-md-5 fs-sm-3 color--white">
+			<?php the_field('intro_copy'); ?>
+		</div>
+	</div>
+</div>
 
 <?php else: ?>
 
-	<div id="<?php echo seoString(get_the_title()); ?>" class="home__hero hero hero--sm wallpaper--parallax bg--black relative" <?php if($thumb_id): ?>style="background-image: url(<?php echo $thumb_url; ?>);" data-stellar-background-ratio="0.75"<?php endif; ?>>
+<div id="<?php echo seoString(get_the_title()); ?>" class="home__hero hero hero--sm wallpaper--parallax bg--black relative" <?php if($thumb_id): ?>style="background-image: url(<?php echo $thumb_url; ?>);" data-stellar-background-ratio="0.75"<?php endif; ?>>
 	<div class="centered centered__bottomer">
 		<div class="fs-row">
 			<div class="fs-cell fs-all-full color--white">
